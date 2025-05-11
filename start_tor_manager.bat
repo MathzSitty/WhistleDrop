9d8fce85d035c062d211c4fe5ea1e5189a28dda6
Marcel <uetkb@student.kit.edu>
2025-05-11 23:59:55 +0200

    Removed unused file.

--- start_tor_manager.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set HTTP_PROXY=socks5h://127.0.0.1:9150
-set HTTPS_PROXY=socks5h://127.0.0.1:9150
-
-REM Adjust path to your virtual environment's python
-D:\WhistleDrop\.venv\Scripts D:\WhistleDrop\utils\tor_manager.py %*
-
-REM Variables are local to this .bat execution
